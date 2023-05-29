f453e352abb4646e8d24a1f4adeba9bd3965ebac
chenwi32 <chenwieugene.j@gmail.com>
2023-05-29 18:53:50 +0100

    added a confetti component

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,7 @@ export default function Home() {
         date: "",
       },
     });
-  const [step, setStep] = useState(6);
+  const [step, setStep] = useState(1);
 
   const updateFormData = (data) => {
     if (step == 1) {
