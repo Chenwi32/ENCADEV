40695874f8af8f61f6e5a1b8abbdf2cca8e295f3
chenwi32 <chenwieugene.j@gmail.com>
2023-06-08 12:31:06 +0100

    fixed headers

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,8 @@ import {
 } from "@chakra-ui/react";
 import "../styles/globals.css";
 import Layout from "../components/layout";
-import Head from "next/head";
 import { AuthContextProvider } from "../components/authcontprov";
+import Head from "next/head";
 
 const theme = extendTheme({
   colors: {
@@ -22,29 +22,12 @@ const theme = extendTheme({
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      
-        <Head>
-          <title>ADES-UK | Forms</title>
-          <meta name="description" content="ADES-UK forms" />
-          <link rel="icon" href="/favicon.ico" />
-          {/* Google  Web Fonts (Outfit) */}
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;400;500;600;700;800&display=swap"
-          />
-
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Andika:ital@0;1&family=Montserrat:wght@200&family=Poppins:wght@200;300&family=Roboto:wght@300&display=swap"
-          />
-        </Head>
-<AuthContextProvider>
+      <Head>
+        <title>ADES-UK | Forms</title>
+        <meta name="description" content="ADES-UK forms" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <AuthContextProvider>
         <ChakraProvider theme={theme}>
           <Layout>
             <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,26 @@ import { Html, Main, Head, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-       <Head />
+      <Head>
+        <meta name="description" content="ADES-UK forms" />
+        <link rel="icon" href="/favicon.ico" />
+        {/* Google  Web Fonts (Outfit) */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;400;500;600;700;800&display=swap"
+        />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Andika:ital@0;1&family=Montserrat:wght@200&family=Poppins:wght@200;300&family=Roboto:wght@300&display=swap"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
